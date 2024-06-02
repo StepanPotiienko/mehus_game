d89633fffca6e34f8ac05b457f92273a5c56fb30
StepanPotiienko <138608284+StepanPotiienko@users.noreply.github.com>
2024-06-02 16:51:13 +0300

    Added a platform I made myself (: | Wrote a not-so-good solution to adding building blocks to inventory.

--- PlayerControls.gd
+++ PlayerControls.gd
@@ -5,9 +5,8 @@ extends CharacterBody3D
 @onready var camera = $Head/Camera3D
 
 
-# Movement settings
+# Movement settings.
 @export var current_speed: float = 5.0
-
 const walking_speed: float = 5.0
 const sprinting_speed: float = 8.0
 
@@ -21,23 +20,22 @@ const sprinting_speed: float = 8.0
 var direction: Vector3 = Vector3.ZERO
 
 
-# Camera settings
+# Camera settings.
 @export var mouse_sensitivity: float = 0.1;
 
 
 @export var ray_length: float = 5.0
 
 
-# Inventory system
+# Inventory system.
 var building_blocks: int = 0
-
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
-# Executes on the start of the game (like Start() method in Unity)
+# Executes on the start of the game (like Start() method in Unity).
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -53,24 +51,24 @@ func _input(event):
 		var collided_object = shoot_ray()
 		
 		if collided_object:
-			if collided_object.name == "BuildingBlock":
+			if collided_object.name in ["BuildingBlock", "BuildingBlock2"]:
 				instance_from_id(collided_object.get_instance_id()).queue_free()
 				building_blocks += 1
 				print(building_blocks)
 
 
 func shoot_ray() -> Object:
-	# Get the size of the viewport (screen size)
+	# Get the size of the viewport (screen size).
 	var viewport = get_viewport()
 	var viewport_size = viewport.size
 	var screen_center = viewport_size / 2
 	
-	# Create a ray from the camera at the center of the screen
+	# Create a ray from the camera at the center of the screen.
 	var camera = viewport.get_camera_3d()
 	var from = camera.project_ray_origin(screen_center)
-	var to = from + camera.project_ray_normal(screen_center) * ray_length  # Arbitrary length
+	var to = from + camera.project_ray_normal(screen_center) * ray_length  # Arbitrary length.
 	
-	# Cast the ray
+	# Cast the ray.
 	var space_state = get_world_3d().direct_space_state
 	var query = PhysicsRayQueryParameters3D.new()
 	query.from = from
@@ -78,7 +76,7 @@ func shoot_ray() -> Object:
 	
 	var result = space_state.intersect_ray(query)
 	
-	# Check if the ray hit something
+	# Check if the ray hit something.
 	if result and result.collider:
 		return result.collider
 	
@@ -90,8 +88,8 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-
+		
+	
 	# Handle jump.
 	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
